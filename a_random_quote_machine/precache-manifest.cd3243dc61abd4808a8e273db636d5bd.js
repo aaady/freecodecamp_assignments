c4e0d3cbd722aab767694b94cadb05b884fd83e0
aaady <aaady403@hotmail.com>
2020-07-18 10:14:09 +0900

    first commit

---
+++ a_random_quote_machine/precache-manifest.cd3243dc61abd4808a8e273db636d5bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363e97ff7bc96472e57d2b7fdfb50a4c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1d194244fc4ba44e638",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85651f527e8bff47e18f",
+    "url": "./static/js/2.f5cb052b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f5cb052b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d194244fc4ba44e638",
+    "url": "./static/js/main.6ba68c02.chunk.js"
+  },
+  {
+    "revision": "f943dc4bceb64cf59a5d",
+    "url": "./static/js/runtime-main.0c7e7982.js"
+  }
+]);
