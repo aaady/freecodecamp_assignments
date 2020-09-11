01d98a1230f7d2271e5954354935f609de4d793c
aaady <aaady403@hotmail.com>
2020-09-11 13:50:55 +0900

    Adding build_a_pomodoro_clock

---
+++ build_a_pomodoro_clock/precache-manifest.01d58bf9591935a6f5f87569928afe09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a381c98165e3ed8ef2ceb7d7d8afbaa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ebf7752b81adfaa679d7",
+    "url": "./static/css/main.720c2dc7.chunk.css"
+  },
+  {
+    "revision": "ea10f332583a5d2acdb8",
+    "url": "./static/js/2.d4ee4c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4ee4c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf7752b81adfaa679d7",
+    "url": "./static/js/main.a745fb97.chunk.js"
+  },
+  {
+    "revision": "6128c0d08fd655663aab",
+    "url": "./static/js/runtime-main.43b7750b.js"
+  }
+]);
